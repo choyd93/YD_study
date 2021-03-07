731a2310c787b3d7fd8b8a95364f801848247d7f
choyd93 <choyd93@naver.com>
2021-03-07 10:25:35 +0900

    chap01애서 sec01 중복되는 폴더 삭제, 리터럴 예제 추가

---
+++ src/sec01/exam01_variable/LiteralExample.java
@@ -0,0 +1,20 @@
+package sec01.exam01_variable;
+
+public class LiteralExample {
+	public static void main(String[] args) {
+		int var1 = 10; //10진수
+		System.out.println(var1);
+		
+		int var2 = 010; //8진수 출력
+		System.out.println(var2);
+		
+		int var3 = 0x10; //16진수 출력
+		System.out.println(var3);
+		
+		double var4 = 0.25;
+		System.out.println(var4);
+		
+		double var5 = 2E5;
+		System.out.println(var5);
+	}
+}
