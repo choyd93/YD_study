c5340250881267013c4f7735950a9e40a2ca6a31
choyd93 <choyd93@naver.com>
2021-03-07 17:57:22 +0900

    이것이 자바다 - 2.2 데이터 타입(2)까지

---
+++ src/sec02/exam03_int/IntExample.java
@@ -0,0 +1,18 @@
+package sec02.exam03_int;
+
+public class IntExample {
+
+	public static void main(String[] args) {
+		
+		int var1 = 10;
+		int var2 = 012; //8진수
+		int var3 = 0xA; //16진수
+		
+		// int var4 = 1000000000;  너무 높은 값은 컴파일 에러 발생, long 타입으로 가능
+		
+		System.out.println(var1);
+		System.out.println(var2);
+		System.out.println(var3);
+	}
+
+}
---
+++ src/sec02/exam05_float_double/FloatDoubleExample.java
@@ -0,0 +1,38 @@
+package sec02.exam05_float_double;
+
+public class FloatDoubleExample {
+
+	public static void main(String[] args) {
+		
+		//실수값 저장
+		double var1 = 3.14;
+		//float var2 = 3.14;
+		float var3 = 3.14F;
+		
+		//정밀도 검사
+		double var4 = 0.1234567890123456789;
+		float var5 = 0.1234567890123456789F;
+		
+		System.out.println(var4);
+		System.out.println(var5);
+		
+		/* 값은
+		0.12345678901234568
+		0.12345679 
+
+		float은 23비트까지 저장
+		double은 52비트까지 저장 */
+		
+		//e사용하기
+		int var6 = 3000000;
+		double var7 = 3e6; //3백만, 부동소수점 방식인 실수타입
+		float var8 = 3e6F; //float타입이라서 뒤에 F 붙임.
+		double var9 = 2e-3;
+		
+		System.out.println(var7);
+		System.out.println(var8);
+		System.out.println(var9);
+		
+	
+	}
+}
